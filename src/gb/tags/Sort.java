5f9b4b914537390c3bd2463454073c57776e170c
Michael Bruckmeier <michael.bruckmeier@googlemail.com>
2011-07-23 17:28:48 +0200

    added desc

--- src/gb/tags/Sort.java
+++ src/gb/tags/Sort.java
@@ -13,75 +13,61 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
 
 public class Sort extends TagSupport {
-	
-	public class ComparatorText implements Comparator<GuestbookEntry>
-	{
-		public int compare(GuestbookEntry a, GuestbookEntry b)
-		{
+
+	public class ComparatorText implements Comparator<GuestbookEntry> {
+		public int compare(GuestbookEntry a, GuestbookEntry b) {
 			return a.getText().compareTo(b.getText());
 		}
 	}
-	
-	public class ComparatorEmail implements Comparator<GuestbookEntry>
-	{
-		public int compare(GuestbookEntry a, GuestbookEntry b)
-		{
+
+	public class ComparatorEmail implements Comparator<GuestbookEntry> {
+		public int compare(GuestbookEntry a, GuestbookEntry b) {
 			return a.getEmail().compareTo(b.getEmail());
 		}
 	}
-	
-	public class ComparatorAuthor implements Comparator<GuestbookEntry>
-	{
-		public int compare(GuestbookEntry a, GuestbookEntry b)
-		{
+
+	public class ComparatorAuthor implements Comparator<GuestbookEntry> {
+		public int compare(GuestbookEntry a, GuestbookEntry b) {
 			return a.getAuthor().compareTo(b.getAuthor());
 		}
 	}
-	
-	public class ComparatorDate implements Comparator<GuestbookEntry>
-	{
-		public int compare(GuestbookEntry a, GuestbookEntry b)
-		{
+
+	public class ComparatorDate implements Comparator<GuestbookEntry> {
+		public int compare(GuestbookEntry a, GuestbookEntry b) {
 			return a.getDate().compareTo(b.getDate());
 		}
 	}
-		
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private String order = "asc";
 	private String attribute = "date";
 	private ArrayList<GuestbookEntry> list = null;
 
 	public int doStartTag() throws JspException {
 		try {
-			if(list!=null && list.size()>0)
-			{
+			if (list != null && list.size() > 0) {
 				final Comparator<GuestbookEntry> c;
-				if(attribute.equals("date"))
-				{
+				if (attribute.equals("date")) {
 					c = new ComparatorDate();
-				}
-				else if(attribute.equals("author"))
-				{
+				} else if (attribute.equals("author")) {
 					c = new ComparatorAuthor();
-				}
-				else if(attribute.equals("email"))
-				{
+				} else if (attribute.equals("email")) {
 					c = new ComparatorEmail();
-				}
-				else
-				{
+				} else {
 					c = new ComparatorText();
-				}				
+				}
 				Collections.sort(list, c);
+				if (order.equals("desc")) {
+					Collections.reverse(list);
+				}
 				StringBuilder entries = new StringBuilder();
-				for(GuestbookEntry ge : list)
-				{
+				for (GuestbookEntry ge : list) {
 					entries.append("<p>");
-					entries.append(ge.getDate());					
+					entries.append(ge.getDate());
 					entries.append("</p><p>");
 					entries.append(ge.getEmail());
 					entries.append("</p><p>");
@@ -92,8 +78,8 @@ public class Sort extends TagSupport {
 				}
 				pageContext.getOut().print(entries);
 			}
-		} catch (Exception ex) {
-			throw new JspTagException("TestTag: " + ex.getMessage());
+		} catch (Exception e) {
+			throw new JspTagException("gb:sort Tag: " + e.getMessage());
 		}
 		return SKIP_BODY;
 	}
@@ -101,57 +87,39 @@ public class Sort extends TagSupport {
 	public int doEndTag() {
 		return EVAL_PAGE;
 	}
-	
-	public void setOrder(String newOrder)
-	{
-		if(newOrder.toLowerCase().equals("desc"))
-		{
+
+	public void setOrder(String newOrder) {
+		if (newOrder.toLowerCase().equals("desc")) {
 			order = "desc";
-		}
-		else
-		{
+		} else {
 			order = "asc";
 		}
 	}
-	
-	public void setAttribute(String newAttribute)
-	{
-		if(newAttribute.equals("author"))
-		{
+
+	public void setAttribute(String newAttribute) {
+		if (newAttribute.equals("author")) {
 			attribute = "author";
-		}
-		else if(newAttribute.equals("text"))
-		{
+		} else if (newAttribute.equals("text")) {
 			attribute = "text";
-		}
-		else if(newAttribute.equals("email"))
-		{
+		} else if (newAttribute.equals("email")) {
 			attribute = "email";
-		}
-		else
-		{
+		} else {
 			attribute = "date";
 		}
 	}
-	
-	public void setList(ArrayList<GuestbookEntry> newList)
-	{
+
+	public void setList(ArrayList<GuestbookEntry> newList) {
 		list = newList;
 	}
-	
-	public static ArrayList<GuestbookEntry> getEntries()
-	{
-		try
-		{
+
+	public static ArrayList<GuestbookEntry> getEntries() {
+		try {
 			Guestbook db = Guestbook.getInstance();
-			if(db!=null)
-			{
+			if (db != null) {
 				return db.getAllEntries();
 			}
+		} catch (Exception ignore) {
 		}
-		catch(Exception ignore)
-		{
-		}
-		return new ArrayList<GuestbookEntry>();	
+		return new ArrayList<GuestbookEntry>();
 	}
 }
